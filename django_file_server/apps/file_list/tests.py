4183fc76fa3d154652880d886b6c2f9be3c2422d
Andrei Korostelev <andrei@korostelev.net>
2018-06-08 20:11:05 +0200

    Extend view test

--- django_file_server/apps/file_list/tests.py
+++ django_file_server/apps/file_list/tests.py
@@ -1,15 +1,31 @@
 # -*- coding: utf-8 -*-
 
-from django.http import HttpRequest
-from django.test import SimpleTestCase
-from django.urls import reverse
+# from django.http import HttpRequest
+from django.test import TestCase
+# from django.test import SimpleTestCase
+# from django.urls import reverse
 
 # from . import views
 
+ADMIN_NAME = 'admin'
+ADMIN_PASSWORD = 'secret'
 
-class FileListViewTests(SimpleTestCase):
 
-    def test_list_files_status_code(self):
-        response = self.client.get('/')
-        self.assertEquals(response.status_code, 200)
+class FileListViewTests(TestCase):
+
+    def test_login(self):
+        #@todo create user? (setup)
+        self.assertTrue(self.client.login(username=ADMIN_NAME, password=ADMIN_PASSWORD))
+        self.assertFalse(self.client.login(username=ADMIN_NAME + '.bad', password=ADMIN_PASSWORD))
+        self.assertFalse(self.client.login(username=ADMIN_NAME, password=ADMIN_PASSWORD + '.bad'))
+
+    def test_request_root_page_when_not_logged_in(self):
+        # first redirect to the file list, then redirects to the login page
+
+        response = self.client.get('/', follow=False)
+        self.assertRedirects(response, '/list/', status_code=301, target_status_code=302)
+
+        response = self.client.get('/', follow=True)
+        self.assertRedirects(response, '/login/?next=/list/', status_code=301, target_status_code=200)
+
 
