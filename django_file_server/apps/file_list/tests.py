a72e487df141ebb66dccdf21679d7f31b7c43b06
Andrei Korostelev <andrei@korostelev.net>
2018-06-10 11:30:41 +0200

    Fix tests

--- django_file_server/apps/file_list/tests.py
+++ django_file_server/apps/file_list/tests.py
@@ -1,31 +1,69 @@
 # -*- coding: utf-8 -*-
 
-# from django.http import HttpRequest
 from django.test import TestCase
-# from django.test import SimpleTestCase
-# from django.urls import reverse
+from django.contrib.auth import get_user_model
+from django.urls import reverse
 
 # from . import views
 
-ADMIN_NAME = 'admin'
-ADMIN_PASSWORD = 'secret'
+TEST_ADMIN_USERNAME = 'admin'
+TEST_ADMIN_EMAIL = 'admin@example.com'
+TEST_ADMIN_PASSWORD = 'secret'
 
 
 class FileListViewTests(TestCase):
+    def setUp(self):
+        super(FileListViewTests, self).setUp()
+        self.admin_user = get_user_model().objects.create_superuser(
+            username=TEST_ADMIN_USERNAME, email=TEST_ADMIN_EMAIL,
+            password=TEST_ADMIN_PASSWORD
+        )
+        self.login()
 
-    def test_login(self):
-        #@todo create user? (setup)
-        self.assertTrue(self.client.login(username=ADMIN_NAME, password=ADMIN_PASSWORD))
-        self.assertFalse(self.client.login(username=ADMIN_NAME + '.bad', password=ADMIN_PASSWORD))
-        self.assertFalse(self.client.login(username=ADMIN_NAME, password=ADMIN_PASSWORD + '.bad'))
+    def tearDown(self):
+        get_user_model().objects.all().delete()
+        super(FileListViewTests, self).tearDown()
 
-    def test_request_root_page_when_not_logged_in(self):
-        # first redirect to the file list, then redirects to the login page
+    def login(self):
+        self.client.login(
+            username=TEST_ADMIN_USERNAME, password=TEST_ADMIN_PASSWORD
+        )
+
+    def logout(self):
+        self.client.logout()
+
+    def test_request_when_not_logged_in(self):
+        # given
+        self.logout()
+
+        # first redirects to the file list, then to the login page
 
         response = self.client.get('/', follow=False)
-        self.assertRedirects(response, '/list/', status_code=301, target_status_code=302)
+        self.assertRedirects(response, reverse('list'), status_code=301, target_status_code=302)
 
         response = self.client.get('/', follow=True)
-        self.assertRedirects(response, '/login/?next=/list/', status_code=301, target_status_code=200)
+        self.assertRedirects(response, '{}?next={}'.format(reverse('login'), reverse('list')), status_code=301, target_status_code=200)
 
+        response = self.client.get(reverse('list'))
+        self.assertRedirects(response, '{}?next={}'.format(reverse('login'), reverse('list')), status_code=302, target_status_code=200)
+
+    def test_request_when_logged_in(self):
+        response = self.client.get('/')
+        self.assertRedirects(response, reverse('list'), status_code=301, target_status_code=200)
+
+        response = self.client.get(reverse('list'))
+        self.assertEquals(response.status_code, 200)
+
+    def test_request_filelist(self):
+        # empty file list
+        response = self.client.get(reverse('list'))
+        self.assertEquals(len(response.context['deliverables']), 0)
+
+        # upload file
+        #@todo permission error
+        # from django.core.files.uploadedfile import SimpleUploadedFile
+        # f = SimpleUploadedFile("file.txt", b"file_content")
+        # response = self.client.post(reverse('list'), {'docfile': f})
+        # self.assertEquals(len(response.context['deliverables']), 1)
+        # print(response.content)
 
--- django_file_server/settings_test.py
+++ django_file_server/settings_test.py
@@ -2,3 +2,8 @@ from .settings_base import *
 
 DEBUG = True
 LOGGING['handlers']['file']['filename'] = 'test.log'
+
+#@todo see STATIC_URL and MEDIA_URL in https://docs.djangoproject.com/en/2.0/howto/static-files/
+# DATABASES['default']['NAME'] = 'test.sqlite3'
+# SENDFILE_ROOT = './media'
+# STATIC_ROOT = './static'
